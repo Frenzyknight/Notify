a83d9bc449f8e048d13fb947f0b2b479c0407ae9
Frenzynknight <gauravganju@gmail.com>
2023-03-15 23:46:08 +0530

    Initial Commit

---
+++ Notify/NotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifyApp.swift
+//  Notify
+//
+//  Created by Gaurav Ganju on 15/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
